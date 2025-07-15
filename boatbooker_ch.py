b587f134d85225145c0f0f6aaf2d9343a39eca0e
Marwan Magdy <engmarwan232@gmail.com>
2025-07-15 18:00:03 +0300

    change the ok button

--- boatbooker_ch.py
+++ boatbooker_ch.py
@@ -12,20 +12,10 @@ USER_DATA_DIR = os.path.abspath("user_data")
 VERIFY_BUTTON_SELECTOR = 'xpath=//*[@id="profile-control"]/div/div/div/div[2]/form/div/div[4]/div/div/div[2]/button'
 PHONE_SELECTOR = 'xpath=//*[@id="phone"]'
 SAVE_CHANGES_SELECTOR = 'xpath=//*[@id="profile-control"]/div/div/div/div[3]/button[1]'
-OK_BUTTON_SELECTOR = 'xpath=/html/body/div[7]/div[7]/div/button'
 
 # List of phone numbers to loop through
 phone_numbers = [
-    50232682960, 50231169628, 50248207388, 50237051328, 50233417740,
-    50257342083, 50246149240, 50231197267, 50240388246, 50230127125,
-    50253807791, 50231769851, 50253099688, 50231888179, 50253605178,
-    50233907270, 50239053218, 50246210589, 50237051229, 50230307852,
-    50232700212, 50230760079, 50231852144, 50232567168, 50248260906,
-    50247940753, 50250052963, 50232407688, 50249387449, 50233120950,
-    50248138493, 50231146557, 50240032952, 50258194059, 50252030210,
-    50230307855, 50246239617, 50245885571, 50249820390, 50231378258,
-    50232200998, 50253268518, 50240352082, 50259906298, 50239082404,
-    50246667326, 50253842084, 50250685767, 50244904189, 50233725019
+    50246667326, 50239082404, 50253842084, 50250685767, 50232682960, 50233725019, 50253605178, 50231769851, 50253807791, 258826466671, 258828771634, 50231197267, 50246149240, 50233417740, 50248207388
 ]
 
 
@@ -68,10 +58,13 @@ with sync_playwright() as p:
                 print("Verify button clicked")
 
                 # Wait for OK dialog and click it
-                page.wait_for_selector(OK_BUTTON_SELECTOR, timeout=10000)
-                print("OK button is now visible")
-                time.sleep(1)
-                page.locator(OK_BUTTON_SELECTOR).click()
+                ok_button = page.get_by_role("button", name="OK")
+                if ok_button.count() > 0 and ok_button.first.is_visible():
+                    ok_button.first.click()
+                    print("OK button clicked")
+                else:
+                    print("No visible OK button found")
+
                 print("OK button clicked")
 
                 # Wait for Verify dialog again and close
